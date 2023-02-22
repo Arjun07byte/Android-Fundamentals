b11ea8fc269f2c12efe7256c2af8239584580c7a
OnlyArjun <arjunyadavcs20@pgdav.du.ac.in>
2023-02-22 15:49:52 +0530

    Final Commit

--- BackgroundReceivers/app/src/main/java/com/appify/backgroundreceivers/MainActivity.kt
+++ BackgroundReceivers/app/src/main/java/com/appify/backgroundreceivers/MainActivity.kt
@@ -2,31 +2,114 @@ package com.appify.backgroundreceivers
 
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.appify.backgroundreceivers.databinding.ActivityMainBinding
 import com.appify.backgroundreceivers.receiverClasses.MainBroadcastReceiver
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var myBroadcastReceiver: MainBroadcastReceiver
+    private lateinit var myViewBinder: ActivityMainBinding
+    private val newIntentFilter = IntentFilter()
+    private var areReceiversRegistered = true
+
+    inner class HomeInterface {
+        fun chargingDisconnected() {
+            myViewBinder.imgViewCharging.alpha =
+                0.1F; myViewBinder.imgViewChargingDisconnected.alpha = 1F
+        }
+
+        fun charging() {
+            myViewBinder.imgViewCharging.alpha =
+                1F; myViewBinder.imgViewChargingDisconnected.alpha = 0.1F
+        }
+
+        fun headsetConnected() {
+            myViewBinder.imgViewHeadset.alpha = 1F; myViewBinder.imgViewHeadsetOff.alpha = 0.1F
+        }
+
+        fun headsetDisconnected() {
+            myViewBinder.imgViewHeadset.alpha = 0.1F; myViewBinder.imgViewHeadsetOff.alpha = 1F
+        }
+
+        fun airplaneOn() {
+            myViewBinder.imgViewAirplaneOn.alpha = 1F; myViewBinder.imgViewAirplaneOff.alpha = 0.1F
+        }
+
+        fun airplaneOff() {
+            myViewBinder.imgViewAirplaneOn.alpha = 0.1F; myViewBinder.imgViewAirplaneOff.alpha = 1F
+        }
+
+        fun timeChanged() {
+            myViewBinder.textViewTime.text = getCurrentTime()
+
+            myViewBinder.textViewTime.alpha = 1F
+            CoroutineScope(Dispatchers.IO).launch {
+                delay(3000); myViewBinder.textViewTime.alpha = 0.1F
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        myViewBinder = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(myViewBinder.root)
 
-        myBroadcastReceiver = MainBroadcastReceiver()
-        val newIntentFilter = IntentFilter()
+        val myHomeInterface = HomeInterface()
+        myBroadcastReceiver = MainBroadcastReceiver(myHomeInterface)
+
+        registerMyBackgroundReceiver(); myViewBinder.textViewTime.text = getCurrentTime()
+
+        // setting up the listener for Stop and Start Button
+        myViewBinder.buttonStopReceivers.setOnClickListener {
+            if (areReceiversRegistered) {
+                unregisterReceiver(myBroadcastReceiver); areReceiversRegistered = false
+                myViewBinder.buttonStopReceivers.text = "Start"
+
+                myViewBinder.layoutChargingReceiver.alpha = 0.2F
+                myViewBinder.layoutHeadsetReceiver.alpha = 0.2F
+                myViewBinder.layoutAirplaneModeReceiver.alpha = 0.2F
+                myViewBinder.layoutTimeReceiver.alpha = 0.2F
+                myViewBinder.textViewErrorMessage.visibility = View.VISIBLE
+            } else {
+                registerMyBackgroundReceiver(); areReceiversRegistered = true
+                myViewBinder.buttonStopReceivers.text = "Stop"
+
+                myViewBinder.layoutChargingReceiver.alpha = 1F
+                myViewBinder.layoutHeadsetReceiver.alpha = 1F
+                myViewBinder.layoutAirplaneModeReceiver.alpha = 1F
+                myViewBinder.layoutTimeReceiver.alpha = 1F
+                myViewBinder.textViewErrorMessage.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun registerMyBackgroundReceiver() {
         newIntentFilter.addAction(Intent.ACTION_POWER_CONNECTED)
         newIntentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
         newIntentFilter.addAction(Intent.ACTION_HEADSET_PLUG)
         newIntentFilter.addAction(Intent.ACTION_TIME_TICK)
         newIntentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+
         newIntentFilter.also {
             registerReceiver(myBroadcastReceiver, it)
         }
     }
 
-    override fun onStop() {
-        super.onStop()
-        unregisterReceiver(myBroadcastReceiver)
+    private fun getCurrentTime(): String {
+        return SimpleDateFormat("hh:mm", Locale.getDefault())
+            .format(Calendar.getInstance().time)
     }
+
+    override fun onStop() {
+        super.onStop(); unregisterReceiver(myBroadcastReceiver)
+    }
+
 }
--- BackgroundReceivers/app/src/main/java/com/appify/backgroundreceivers/receiverClasses/MainBroadcastReceiver.kt
+++ BackgroundReceivers/app/src/main/java/com/appify/backgroundreceivers/receiverClasses/MainBroadcastReceiver.kt
@@ -3,34 +3,29 @@ package com.appify.backgroundreceivers.receiverClasses
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
+import com.appify.backgroundreceivers.MainActivity
 
-class MainBroadcastReceiver : BroadcastReceiver() {
+class MainBroadcastReceiver(private val homeInterface: MainActivity.HomeInterface) :
+    BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
-            Intent.ACTION_POWER_CONNECTED -> {
-                Toast.makeText(context, "Phone is Charging", Toast.LENGTH_LONG).show()
-            }
-            Intent.ACTION_POWER_DISCONNECTED -> {
-                Toast.makeText(context, "Charging Disconnected", Toast.LENGTH_LONG).show()
-            }
+            Intent.ACTION_POWER_CONNECTED -> homeInterface.charging()
+            Intent.ACTION_POWER_DISCONNECTED -> homeInterface.chargingDisconnected()
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
                 if (intent.getBooleanExtra("state", false)) {
-                    Toast.makeText(context, "Airplane Mode On", Toast.LENGTH_LONG).show()
+                    homeInterface.airplaneOn()
                 } else {
-                    Toast.makeText(context, "Airplane Mode Off", Toast.LENGTH_LONG).show()
+                    homeInterface.airplaneOff()
                 }
             }
             Intent.ACTION_HEADSET_PLUG -> {
                 if (intent.getIntExtra("state", 0) == 1) {
-                    Toast.makeText(context, "Headset Plugged", Toast.LENGTH_LONG).show()
+                    homeInterface.headsetConnected()
                 } else {
-                    Toast.makeText(context, "Headset Unplugged", Toast.LENGTH_LONG).show()
+                    homeInterface.headsetDisconnected()
                 }
             }
-            Intent.ACTION_TIME_TICK -> {
-                Toast.makeText(context, "Time has Changed", Toast.LENGTH_LONG).show()
-            }
+            Intent.ACTION_TIME_TICK -> homeInterface.timeChanged()
         }
     }
 }
